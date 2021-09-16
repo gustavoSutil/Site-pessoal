7e87a0678f2229b7e9aed8c2ba6fed70a461738d
Gustavo_PJL_da Quebrada <4541082348@estudante.sed.sc.gov.br>
2021-09-16 02:45:16 -0300

    +1

--- initial/funcionalidades.js
+++ initial/funcionalidades.js
@@ -1,7 +1,18 @@
-var contagem=0
+var contagem=0;
 function nav(numero){
     contagem=contagem+numero
     if (contagem%2==0){
-        console.log("1")
+        apagar()
     }
+    else{
+        inserir()
+    }
+}
+function inserir(){
+    var opcoes= document.getElementById("menu"); 
+    return opcoes.insertAdjacentHTML("afterbegin","<span id='span' style='color:white;list-style: none;background: #000010;height: 400px;transform-style: flat;justify-content: flex-end;' ><article id='opcoes'><li>Contato</li><li>Carreira</li><li>Mais informações</li></article></span>");
+    }
+function apagar(){
+    var sair= document.getElementById('span');
+     return sair.remove()
 }
